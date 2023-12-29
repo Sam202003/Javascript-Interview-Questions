008f6cedcb4b7fbebb179815483c7f9207401a12
Sam202003 <samruddhichavan05@gmai.com>
2023-12-29 20:28:54 +0530

    added this

---
+++ this/index.js
@@ -0,0 +1,19 @@
+//this keyword in Javascript
+
+//Explain 'this' keyword
+// In JS this keyword is used to give refrence something like an object.
+
+//Functions are first class objects.
+
+//Here this is like a global context
+
+// this.a = 5;
+// console.log(this.a); //5
+// console.log(this); //{ a: 5 }
+
+
+this.a = 5;
+function getParams() {
+console.log(this.a);
+}
+getParams();
